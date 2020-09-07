0d6ace6bdaf561cc4704609ee27c766f1f451100
Ariel <a.t.navarro.1993@gmail.com>
2020-09-07 20:53:05 -0300

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+console.log('test de '+document.title)
+
+function expander(x,bool){
+    bool? x.style.width = "100px" : x.style.width = "50px";
+}
+
+fetch('https://api.getform.io/v1/forms/97727371-d58f-46ef-a827-dab4209b654a?token=NqibkSahVCDYgI91bhHW28VWNrfrNpt6GpGVMAKXzL5rY5du74z5LxSBN6Bo')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
